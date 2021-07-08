9575d70e6baf7b74de6e5e3ec7857cb1dbf98cb1
Samuel Gonçalves <75588877+Samuel-Goncalves5@users.noreply.github.com>
2021-07-08 09:14:18 +0200

    Ajout Réseau et 3D

---
+++ Les Anges de la Nuit - Classique/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using UnityEngine;
+using Photon.Pun;
+public class PlayerController : MonoBehaviour
+{
+    PhotonView view;
+
+    private void Start()
+    {
+        view = GetComponent<PhotonView>();
+    }
+
+    void Update()
+    {
+        if (!view.IsMine) return;
+        
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(0, 0, -5 * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(0, 0, 2.5f * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(0, 200 * Time.deltaTime, 0);
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(0, -200 * Time.deltaTime, 0);
+        }
+    }
+}
---
+++ Les Anges de la Nuit - Classique/Assets/Scripts/SpawnPlayers.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Photon.Pun;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class SpawnPlayers : MonoBehaviour
+{
+    public GameObject playerPrefab;
+
+    private void Start()
+    {
+        Vector3 Position = new Vector3(Random.Range(-15,15), 2, Random.Range(-15,15));
+        PhotonNetwork.Instantiate(playerPrefab.name, Position, Quaternion.identity);
+    }
+}
